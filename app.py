12286fa95c6ea89f3ff736b4cc7860a0f93737f0
Nidhi18102002 <nidhinagathan1810@gmail.com>
2025-07-30 23:34:44 +0530

    step 3 adding name list

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect, url_for
+
 
 app = Flask(__name__)
+submitted_name= []
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         name = request.form["name"]
+        submitted_name.append(name)
+        return redirect (url_for("names"))
         return f"<h1>Hello, {name}!</h1>"
     
     return '''
@@ -14,6 +19,10 @@ def home():
             <button type="submit">Submit</button>
         </form>
     '''
+@app.route("/names")
+def names():
+    return render_template("names.html", names=submitted_name)
+
 @app.route("/about")
 def about():
     return render_template("about.html")
